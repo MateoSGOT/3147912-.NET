5faf30e8bc24e7e1407ca2fc087ac1567d3771a4
mateo <materpatino601@gmail.com>
2025-12-01 16:28:27 -0500

    resuelto conflicto

---
+++ Crud Nativo/Crud Nativo/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud_Nativo.Models
+{
+    public class Producto
+    {
+        [Key] public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime fechaCreacion { get; set; }
+
+
+
+    }
+}
---
+++ modificadores/modificadores/Vehiculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modificadores
+{
+    internal class Vehiculo
+    {
+        //atributos privados
+        private string marca { get; set; }
+        private string modelo { get; set; }
+        private int kilomatraje { get; set; }
+
+        public Vehiculo(string marca, string modelo, int kilomatraje)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.kilomatraje = kilomatraje;
+        }
+
+        protected void CostoMantenimiento()
+        {
+           
+        }
+
+    }
+}
